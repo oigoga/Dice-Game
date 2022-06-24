9d02412c77bd7675fa7551ec137aa945b480a643
oigoga <gogaelisabeth21@gmail.com>
2022-06-24 08:03:10 +0100

    2nd Commit

--- starter/script.js
+++ starter/script.js
@@ -1,53 +1,57 @@
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 function init() {
-    scores = [0, 0];
-    roundScore = 0;
-    activePlayer = 0;
-    document.querySelector('.dice').style.display = 'none';
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-document.getElementById('name--0' ).textContent = 'Player 1';
-document.getElementById('name--1' ).textContent = 'Player 2';
-document.querySelector('.player--0').classList.remove('player--active');
-document.querySelector('.player--1').classList.remove('player--active');
-document.querySelector('.player--0').classList.remove('player--winner');
-document.querySelector('.player--1').classList.remove('player--winner');
+  gamePlaying = true;
 
-document.querySelector('.player--0').classList.add('player--active');
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name--0').textContent = 'Player 1';
+  document.getElementById('name--1').textContent = 'Player 2';
+  document.querySelector('.player--0').classList.remove('player--active');
+  document.querySelector('.player--1').classList.remove('player--active');
+  document.querySelector('.player--0').classList.remove('player--winner');
+  document.querySelector('.player--1').classList.remove('player--winner');
+
+  document.querySelector('.player--0').classList.add('player--active');
 }
- init();
+init();
 
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = dice;
 
-
-
 document.querySelector('.btn-roll').addEventListener('click', function () {
-  // 1. Random Number
-  var dice = Math.floor(Math.random() * 6) + 1;
+  if (gamePlaying) {
+    // 1. Random Number
+    var dice = Math.floor(Math.random() * 6) + 1;
 
-  // 2. Display the result
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
-  diceDOM.src = 'dice-' + dice + '.png';
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
-  // 3. Update the round score if the rolled number was not a 1
+    // 3. Update the round score if the rolled number was not a 1
 
-  if (dice !== 1) {
-    // Add Score
-    roundScore += dice;
-    document.querySelector('#current-' + activePlayer).textContent = roundScore;
-  } else {
-    // Next - Player
-    nextPlayer();
+    if (dice !== 1) {
+      // Add Score
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent =
+        roundScore;
+    } else {
+      // Next - Player
+      nextPlayer();
+    }
   }
 });
 
 document.querySelector('.btn--hold').addEventListener('click', function () {
-  // Add current score to global score
+  if (gamePlaying){
+    // Add current score to global score
   scores[activePlayer] += roundScore;
 
   // Update the UI
@@ -64,10 +68,13 @@ document.querySelector('.btn--hold').addEventListener('click', function () {
     document
       .querySelector('.player--' + activePlayer)
       .classList.remove('active');
+      gamePlaying = false;
   } else {
     // Next - Player
     nextPlayer();
   }
+  }
+  
 });
 
 function nextPlayer() {
